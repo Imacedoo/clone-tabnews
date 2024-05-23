b9f9282a056561636a218d49ec729ff4d16d676e
Igor Prates <imacedo2000.im@gmail.com>
2024-05-23 15:25:06 -0300

    finaliza `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnection = databaseOpenedConnectionResult.rows[0].count;
+  console.log(databaseOpenedConnection);
   response.status(200).json({
-    message: "Eai!",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnection,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,23 @@
 describe("GET to /api/v1/status", () => {
   test("should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-
     expect(response.status).toBe(200);
+
+    const {
+      updated_at,
+      dependencies: {
+        database: { version, max_connections, opened_connections },
+      },
+    } = await response.json();
+
+    expect(updated_at).toBeDefined();
+
+    const parsedUpdatedAt = new Date(updated_at).toISOString();
+    expect(updated_at).toEqual(parsedUpdatedAt);
+
+    expect(version).toMatch(/\b\d{1,2}\.\d{1,2}\b/);
+    expect(typeof max_connections).toBe("number");
+    // Nesse controller, nessa situação, é esperado 1 conexão apenas, se tiver mais está vazando.
+    expect(opened_connections).toEqual(1);
   });
 });

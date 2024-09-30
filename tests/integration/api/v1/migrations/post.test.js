9986a78e34e736b6adcae5e43322ece68240bab7
Igor Prates <imacedo2000.im@gmail.com>
2024-09-30 16:17:38 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,28 +6,34 @@ beforeAll(async () => {
   await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
 });
 
-describe("POST to /api/v1/migrations", () => {
-  test("should return 200", async () => {
-    const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For first time", async () => {
+        const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+
+        expect(response1.status).toBe(201);
+
+        const response1Body = await response1.json();
+
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+      });
+
+      test("For second time", async () => {
+        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
     });
-
-    expect(response1.status).toBe(201);
-
-    const response1Body = await response1.json();
-
-    expect(Array.isArray(response1Body)).toBe(true);
-    expect(response1Body.length).toBeGreaterThan(0);
-
-    const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
-    });
-
-    expect(response2.status).toBe(200);
-
-    const response2Body = await response2.json();
-
-    expect(Array.isArray(response2Body)).toBe(true);
-    expect(response2Body.length).toBe(0);
   });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,26 +4,28 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-describe("GET to /api/v1/status", () => {
-  test("should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-    const {
-      updated_at,
-      dependencies: {
-        database: { version, max_connections, opened_connections },
-      },
-    } = await response.json();
+      const {
+        updated_at,
+        dependencies: {
+          database: { version, max_connections, opened_connections },
+        },
+      } = await response.json();
 
-    expect(updated_at).toBeDefined();
+      expect(updated_at).toBeDefined();
 
-    const parsedUpdatedAt = new Date(updated_at).toISOString();
-    expect(updated_at).toEqual(parsedUpdatedAt);
+      const parsedUpdatedAt = new Date(updated_at).toISOString();
+      expect(updated_at).toEqual(parsedUpdatedAt);
 
-    expect(version).toMatch(/\b\d{1,2}\.\d{1,2}\b/);
-    expect(typeof max_connections).toBe("number");
-    // Nesse controller, nessa situação, é esperado 1 conexão apenas, se tiver mais está vazando.
-    expect(opened_connections).toEqual(1);
+      expect(version).toMatch(/\b\d{1,2}\.\d{1,2}\b/);
+      expect(typeof max_connections).toBe("number");
+      // Nesse controller, nessa situação, é esperado 1 conexão apenas, se tiver mais está vazando.
+      expect(opened_connections).toEqual(1);
+    });
   });
 });

411c9bd90a3ab51fa3b77e5069f64c0107aeef45
Igor Prates <imacedo2000.im@gmail.com>
2025-12-16 16:01:05 -0300

    feat: invalidate session cookie on unauthorized error

--- infra/controller.js
+++ infra/controller.js
@@ -9,11 +9,12 @@ import {
 } from "./errors";
 
 function onErrorHandler(error, request, response) {
-  if (
-    error instanceof ValidationError ||
-    error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
-  ) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return response.status(error.statusCode).json(error);
+  }
+
+  if (error instanceof UnauthorizedError) {
+    clearSessionCookie(response);
     return response.status(error.statusCode).json(error);
   }
 

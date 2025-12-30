9b5a24ed869ea2109d3016f74180d2a98e61effa
Igor Prates <imacedo2000.im@gmail.com>
2025-12-29 21:29:05 -0300

    fix: handle empty email list in `orchestrator.getLastEmail()`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -91,6 +91,8 @@ async function getLastEmail() {
   const emailListBody = await emailListResponse.json();
   const lastEmailItem = emailListBody.pop();
 
+  if (!lastEmailItem) return null;
+
   const emailTextResponse = await fetch(`${EMAIL_HTTP_URL}/messages/${lastEmailItem.id}.plain`);
   const emailTextBody = await emailTextResponse.text();
 

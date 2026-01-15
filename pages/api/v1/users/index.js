f6838dcab22507552683169df103635df55f200d
Igor Prates <imacedo2000.im@gmail.com>
2026-01-15 17:26:32 -0300

    feat: require `create:user` to access `/api/v1/users`

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -5,7 +5,9 @@ import activation from "models/activation";
 
 const router = createRouter();
 
-router.post(postHandler);
+router
+  .use(controller.injectAnonymousOrUser)
+  .post(controller.canRequest("create:user"), postHandler);
 
 export default router.handler(controller.errorHandlers);
 
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -128,4 +128,36 @@ describe("POST /api/v1/users", () => {
       });
     });
   });
+
+  describe("Default user", () => {
+    test("With unique and valid data", async () => {
+      const user1 = await orchestrator.createUser();
+      await orchestrator.activateUser(user1);
+      const user1ObjectSession = await orchestrator.createSession(user1.id);
+
+      const user2Response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${user1ObjectSession.token}`,
+        },
+        body: JSON.stringify({
+          name: "usuariologado",
+          email: "usuariologado@gmail.com",
+          password: "senhasegura",
+        }),
+      });
+
+      expect(user2Response.status).toBe(403);
+
+      const user2ResponseBody = await user2Response.json();
+
+      expect(user2ResponseBody).toEqual({
+        name: "ForbiddenError",
+        message: "Você não possui permissão para executar essa ação",
+        action: 'Verifique se o seu usuário possui a feature "create:user"',
+        status_code: 403,
+      });
+    });
+  });
 });
